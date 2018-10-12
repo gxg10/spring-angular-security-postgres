b69c26e66dc5245d149dd6328ad9a8896fc4fed5
George Brande <george.brande@gmail.com>
2018-10-12 12:55:53 +0300

    init OK

---
+++ angular/src/app/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  userName: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    const url = 'http://localhost:8082/user';
+
+    const headers: HttpHeaders = new HttpHeaders({
+        'Authorization': 'Basic ' + sessionStorage.getItem('token')
+    });
+
+    if (sessionStorage.getItem('token').length > 10) {
+        console.log('patatata ');
+        const options = { headers: headers };
+    this.http.post<Observable<Object>>(url, {}, options)
+        .subscribe(
+            data => {
+                this.userName = data['name'];
+            },
+            error => {
+                if (error.status === 0) {
+                    alert('Unauthorized');
+                }
+                console.log(error.status);
+            }
+        );
+    }
+
+  }
+
+  logout() {
+      sessionStorage.setItem('token', '');
+  }
+  private handleError(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        console.error('An error occurred:', error.error.message);
+      } else {
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      return 'Something bad happened; please try again later.')
+    }
+}
---
+++ spring/src/main/java/com/example/spring/Application.java
@@ -0,0 +1,15 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication(scanBasePackages = "com.example.spring")
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
